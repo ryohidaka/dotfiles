815520103ca452af96ec74cc79a7f6b3dc88a251
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-11-01 22:19:12 +0900

    feat: Add uninstall-homebrew script.

---
+++ uninstall/scripts/uninstall-homebrew.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# List of directories to be deleted
+directories=(/usr/local/Frameworks/
+/usr/local/Homebrew/
+/usr/local/bin/
+/usr/local/etc/
+/usr/local/include/
+/usr/local/lib/
+/usr/local/n/
+/usr/local/opt/
+/usr/local/sbin/
+/usr/local/share/
+/usr/local/var/
+~/Brewfile
+~/Brewfile.lock.json)
+
+# Delete the specified directories
+for dir in "${directories[@]}"; do
+    sudo rm -rf "$dir"
+    if [ $? -eq 0 ]; then
+        echo "[HomeBrew] Deletion of $dir successful"
+    else
+        echo "[HomeBrew] Deletion of $dir failed"
+    fi
+done
