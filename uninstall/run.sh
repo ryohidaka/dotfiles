d2ce67e7f952a0e9175e11967120cbd42c905685
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-11-01 22:19:12 +0900

    feat: Add uninstall-homebrew script.

--- uninstall/run.sh
+++ uninstall/run.sh
@@ -4,3 +4,7 @@ DIR=./uninstall/scripts
 
 # Delete chezmoi cache
 sh $DIR/remove-chezmoi-cache.sh
+
+# Uninstall homebrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
+sudo sh $DIR/uninstall-homebrew.sh
