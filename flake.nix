c9cf09b9b9b9d84356e12d8efcbb0af4758a3f2e
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2026-04-09 23:16:03 +0900

    feat(nix): enable nix-darwin

--- flake.nix
+++ flake.nix
@@ -1,11 +1,28 @@
 {
-  description = "nix system flake";
+  description = "nix-darwin system flake";
+
   inputs = {
+    # Nix package collection (unstable channel)
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    # nix-darwin: macOS system configuration via Nix
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
   outputs =
-    { ... }:
+    inputs@{ self, nixpkgs, ... }:
+    let
+      # Load helper functions from lib/
+      lib = import ./lib inputs;
+
+      # Host definitions
+      hosts = { };
+    in
     {
-      # TBD
+      # Build a darwinConfiguration for each host
+      darwinConfigurations = builtins.mapAttrs lib.mkDarwinSystem hosts;
     };
 }
